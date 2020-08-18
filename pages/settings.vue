f264cf4fd610332c5009651f6e80029709d7851a
Hoon <dt@live.it>
2020-08-18 19:10:48 +0800

    编辑资料用户体验优化

--- pages/settings.vue
+++ pages/settings.vue
@@ -13,13 +13,13 @@
       <tr>
         <td>昵称</td>
         <td>
-          <input type="text" v-model='user_nick'/>
+          <input type="text" v-model="user_nick" />
         </td>
       </tr>
       <tr>
         <td>介绍</td>
         <td>
-          <textarea v-model='user_desc'></textarea>
+          <textarea v-model="user_desc"></textarea>
         </td>
       </tr>
       <tr>
@@ -35,77 +35,84 @@
       <tr>
         <td>生日</td>
         <td>
-          <el-date-picker v-model="date" type="date" placeholder="选择日期"></el-date-picker>
+          <el-date-picker
+            v-model="date"
+            type="date"
+            placeholder="选择日期"
+          ></el-date-picker>
         </td>
       </tr>
       <tr>
-        <td>
-        </td>
+        <td></td>
         <td>
           <el-button type="primary" @click="handleSave">保存</el-button>
         </td>
       </tr>
-      <!-- <tr>
-        <td>地区</td>
-        <td>
-          <el-cascader
-            size="large"
-            :options="options"
-            v-model="selectedOptions"
-          ></el-cascader>
-        </td>
-      </tr> -->
     </table>
   </div>
 </template>
 
 <script>
 // import { provinceAndCityData } from 'element-china-area-data';
-import uploadImg from "@/components/uploadImg"
+import uploadImg from "@/components/uploadImg";
 export default {
   data() {
     return {
       // options: provinceAndCityData,
       // selectedOptions: [],
       date: "",
-      gender: 1,
-      user_desc:"",
-      user_nick:"",
-      avatar:null
+      gender: parseInt(this.$store.state.user.user_sex),
+      user_desc: this.$store.state.user.user_desc,
+      user_nick: this.$store.state.user.user_nick,
+      avatar: null
     };
   },
-  components:{uploadImg},
-  computed:{
-    user_pic(){
-      return this.$store.state.pic
+  components: { uploadImg },
+  computed: {
+    user_pic() {
+      return this.$store.state.pic;
     }
   },
-  methods:{
-    async handleSave(){
-      if(this.user_nick.len != 0){
-        const data = new FormData()
-        data.append("user_nick",this.user_nick)
-        data.append("user_desc",this.user_desc)
-        data.append("user_sex",this.gender)
-        data.append("user_name",this.$store.state.userName)
-        if(this.avatar !== null){
-          data.append("user_avatar",this.avatar)
+  methods: {
+    async handleSave() {
+      if (this.user_nick.len != 0) {
+        const data = new FormData();
+        data.append("user_nick", this.user_nick);
+        data.append("user_desc", this.user_desc);
+        data.append("user_sex", this.gender);
+        data.append("user_name", this.$store.state.userName);
+        if (this.avatar !== null) {
+          data.append("user_avatar", this.avatar);
         }
-        await this.$axios.post(`${process.env.BACKEND_URL}/update/ucenter`,data);
+        await this.$axios.post(
+          `${process.env.BACKEND_URL}/update/ucenter`,
+          data
+        );
         this.$message({
           message: "修改成功！",
           type: "success"
         });
         this.reloadAvatar();
+        this.reloadUser();
         this.$router.push(`/user/${this.$store.state.userName}`);
       }
     },
-    async reloadAvatar(){
-      const result = await this.$axios.post(`${process.env.BACKEND_URL}/get/user_pic`,{user_name:this.$store.state.userName});
-      this.$store.commit("setUserPic",result.data.user_pic);
+    async reloadAvatar() {
+      const result = await this.$axios.post(
+        `${process.env.BACKEND_URL}/get/user_pic`,
+        { user_name: this.$store.state.userName }
+      );
+      this.$store.commit("setUserPic", result.data.user_pic);
       // console.log(result);
     },
-    setImg(file){
+    async reloadUser() {
+      const result = await this.$axios.post(
+        `${process.env.BACKEND_URL}/get/user`,
+        { user_name: this.$store.state.userName }
+      );
+      this.$store.commit("setUser", result.data[0]);
+    },
+    setImg(file) {
       this.avatar = file;
     }
   }
