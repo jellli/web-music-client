fea86ee892859f4fd696e3d2ec38046c55ff9606
Hoon <dt@live.it>
2020-08-19 11:14:53 +0800

    修复歌词显示不正常的bug

--- components/lyric.vue
+++ components/lyric.vue
@@ -39,9 +39,11 @@ export default {
           let obj = {};
           let time_arr = element.substr(1, element.length - 1).split(":"); //先把多余的“[”去掉，再分离出分、秒
           let s = parseInt(time_arr[0]) * 60 + Math.round(time_arr[1]); //把时间转换成与currentTime相同的类型，方便待会实现滚动效果
-          obj.time = s;
-          obj.text = text;
-          this.lyric.push(obj); //每一行歌词对象存到组件的lyric歌词属性里
+          if (text !== "") {
+            obj.time = s;
+            obj.text = text;
+            this.lyric.push(obj); //每一行歌词对象存到组件的lyric歌词属性里
+          }
         });
       }
     }
