c754a87264a107a208f57a66f524afcd675697dc
Hoon <dt@live.it>
2020-08-25 13:33:42 +0800

    更改作者头像链接

--- components/foot.vue
+++ components/foot.vue
@@ -14,13 +14,13 @@
       <a href="https://github.com/Greyy-Y" title="Greyy-Y">
         <el-avatar
           size="small"
-          src="https://avatars0.githubusercontent.com/u/59171310?v=4"
+          src="https://web-music.oss-cn-shenzhen.aliyuncs.com/avatar/grey.jpg"
         ></el-avatar>
       </a>
       <a href="https://github.com/hothoney" title="hothoney">
         <el-avatar
           size="small"
-          src="https://avatars2.githubusercontent.com/u/22422393?v=4"
+          src="https://web-music.oss-cn-shenzhen.aliyuncs.com/avatar/hoon.jpg"
         ></el-avatar>
       </a>
     </div>
